2cba53c24c9fd6a5b582be5ef79ccb6574bbdd55
lisaEpifanova <lisa.yepifanova@gmail.com>
2017-07-14 15:57:48 +0300

    contacts styled

--- js/scripts/contact-items.js
+++ js/scripts/contact-items.js
@@ -54,36 +54,4 @@
         contactItemResize()
     });
 
-}());;;// Avoid `console` errors in browsers that lack a console.
-(function() {
-    var method;
-    var noop = function () {};
-    var methods = [
-        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
-        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
-        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
-        'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
-    ];
-    var length = methods.length;
-    var console = (window.console = window.console || {});
-
-    while (length--) {
-        method = methods[length];
-
-        // Only stub undefined methods.
-        if (!console[method]) {
-            console[method] = noop;
-        }
-    }
 }());
-
-// Place any jQuery/helper plugins in here.
-;var hammer = new Hammer(document.querySelector('.carousel'));
-var $carousel = $(".carousel").carousel({"interval": 0});
-hammer.get("swipe");
-hammer.on("swipeleft", function () {
-    $carousel.carousel("next");
-});
-hammer.on("swiperight", function () {
-    $carousel.carousel("prev");
-});
