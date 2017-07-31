f48458055cc0780af2ec43e5e47962ccc4711add
lisaEpifanova <lisa.yepifanova@gmail.com>
2017-07-31 16:30:30 +0300

    added info on the page contact, add it to the db

--- application/models/model_contact.php
+++ application/models/model_contact.php
@@ -1,10 +1,29 @@
 <?php
+
 class Model_Contact extends Model
 {
     public function get_data()
     {
-        return array(
-          'map' => 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d28779.29598554028!2d32.078323346655644!3d49.44390371448328!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xbc334d7c335631fe!2z0L_QsNGA0Log0JTQvtC70LjQvdCwINCi0YDQvtGP0L3QtA!5e0!3m2!1suk!2sua!4v1500081087022'
-        );
+        include 'application/connection.php';
+        $query = $mysqli->query("SELECT * FROM contact_page");
+
+        if ($query) {
+            while ($r = mysqli_fetch_assoc($query)) {
+                $res = $r;
+            }
+        }
+
+
+        $query = $mysqli->query("SELECT * FROM main_contact_page_info");
+
+        if ($query) {
+            while ($r = mysqli_fetch_assoc($query)) {
+                $res['main_info'][] = $r['info'];
+            }
+        }
+
+
+
+        return $res;
     }
 }
--- application/views/contact_view.php
+++ application/views/contact_view.php
@@ -4,5 +4,34 @@
 
   <iframe
       src=" <?php echo $data['map'] ?>"
-      class="gmap"  allowfullscreen></iframe>
+      class="gmap" allowfullscreen></iframe>
+  <div class="container contacts-info-wrapper box-mid-margin right-padding">
+
+    <div class="company-names">
+      <h5 class="container-info-title"><?php echo $data['company_name'] ?></h5>
+      <h5 class="container-info-title"><?php echo $data['second_company_name'] ?></h5>
+    </div>
+
+    <div class="contact-main-info">
+        <?php
+        foreach ($data['main_info'] as $row) {
+            echo '<p class="main-info-item">'.$row.'</p>';
+        }
+        ?>
+    </div>
+    <div class="copyright">
+      <h5 class="container-info-title">Copyright ©</h5>
+      <div class="copyright-info-text"><?php echo $data['copyright'] ?></div>
+    </div>
+
+    <div class="externe_links">
+      <h5 class="container-info-title">Externe Links</h5>
+      <div class="copyright-info-text"><?php echo $data['externe_links'] ?></div>
+    </div>
+
+    <div class="erklarung">
+      <h5 class="container-info-title">Erklärung</h5>
+      <div class="copyright-info-text"><?php echo $data['erklarung'] ?></div>
+    </div>
+  </div>
 </main>
