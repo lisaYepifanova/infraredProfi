e0f0ef516117f2898035478321a97c8dcc60cb26
lisaEpifanova <lisa.yepifanova@gmail.com>
2017-08-16 20:24:23 +0300

    fixed product text

--- js/build.js
+++ js/build.js
@@ -269,25 +269,29 @@ $('.homepage-gallery-carousel').carousel({
     $productArrow = '.product-full-description-wrapper .arrow-down';
     $container = $('html, body');
 
-    $($productArrow).on('click', function () {
+    if ( parseInt($('.product-full-description').css('height')) > "200"  ) {
+        $($productArrow).on('click', function () {
 
 
-        if ($($textWrapper).prop('scrollHeight') > $($textWrapper).height()) {
-            $maxH = parseInt($($textWrapper).prop('scrollHeight')) + 50;
-            $($textWrapper).animate({maxHeight: $maxH + 'px'}, 1200);
-            $($productArrow).css('position', 'relative');
-            $($productArrow).css('padding', '0');
-            $($productArrow).css('transform', 'scaleY(-1)');
+            if ($($textWrapper).prop('scrollHeight') > $($textWrapper).height()) {
+                $maxH = parseInt($($textWrapper).prop('scrollHeight')) + 50;
+                $($textWrapper).animate({maxHeight: $maxH + 'px'}, 1200);
+                $($productArrow).css('position', 'relative');
+                $($productArrow).css('padding', '0');
+                $($productArrow).css('transform', 'scaleY(-1)');
 
-        } else {
-            $($textWrapper).animate({maxHeight: '200px'}, 1200);
-            $($productArrow).css('position', 'absolute');
-            $($productArrow).css('padding', '1rem');
-            $($productArrow).css('transform', 'scaleY(1)');
+            } else {
+                $($textWrapper).animate({maxHeight: '200px'}, 1200);
+                $($productArrow).css('position', 'absolute');
+                $($productArrow).css('padding', '1rem');
+                $($productArrow).css('transform', 'scaleY(1)');
 
-            $('html, body').animate({scrollTop: $('.product-description').offset().top}, 1200);
-        }
-    });
+                $('html, body').animate({scrollTop: $('.product-description').offset().top}, 1200);
+            }
+        });
+    } else {
+        $($productArrow).css('display', 'none');
+    }
 }());
 ;function arrowsFaqRotation($accTitle, $tex) {
     $($accTitle).on('click', function () {
